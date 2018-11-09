b785931b187e221334bbf6de90a74a2ca9c2c8a1
pachecoSen <pachecodr2013@gmail.com>
2018-11-09 13:39:08 -0600

    f0002. Hola_Mundo - Saludo

---
+++ curso/src/app/components/hello/hello.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  templateUrl: './templete/hello.component.html',
+  styleUrls: ['./assets/hello.component.css']
+})
+export class AppComponent {
+  title = 'Hola Mundo!';
+}
