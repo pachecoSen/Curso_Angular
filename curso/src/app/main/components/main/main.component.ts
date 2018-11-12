714dd5e51897b82a039a0bbc6af2e2e147eda2f0
pachecoSen <pachecodr2013@gmail.com>
2018-11-12 14:21:30 -0600

    f0001. Ejercisios Dos. Tipo de datos

--- curso/src/app/main/components/main/main.component.ts
+++ curso/src/app/main/components/main/main.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'main-app-root',
-  templateUrl: './assets/main.component.html',
-  styleUrls: ['./assets/main.component.css']
+	selector: 'main-app-root',
+	templateUrl: './assets/main.component.html',
+	styleUrls: ['./assets/main.component.css']
 })
 export class MainComponent {
-  title = 'curso';
+	public nombre:string = 'Ejercios Datos y propiedades';
+	public edad:number = 15;
+	public estatus:boolean = true;
+	public materias:Array<String> = ['Matematicas', 'Español', 'Historia'];
+	comodin:any = 'Lo que sea es este parametro';
 }
