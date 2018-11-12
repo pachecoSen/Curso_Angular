663284e5054fa3ac186b62d6c892c7b5530ee260
pachecoSen <pachecodr2013@gmail.com>
2018-11-12 13:50:48 -0600

    f0005. Estructura Base

---
+++ curso/src/app/main/components/main/main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-app-root',
+  templateUrl: './assets/main.component.html',
+  styleUrls: ['./assets/main.component.css']
+})
+export class MainComponent {
+  title = 'curso';
+}
