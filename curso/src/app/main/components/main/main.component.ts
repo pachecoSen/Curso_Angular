117fa6694c3d75f3542273bea40aad19c922d56b
pachecoSen <pachecodr2013@gmail.com>
2018-11-12 16:54:09 -0600

    f0002. Ejercisios Dos. FUnciones, Contructor y ngOnInit

--- curso/src/app/main/components/main/main.component.ts
+++ curso/src/app/main/components/main/main.component.ts
@@ -6,9 +6,48 @@ import { Component } from '@angular/core';
 	styleUrls: ['./assets/main.component.css']
 })
 export class MainComponent {
-	public nombre:string = 'Ejercios Datos y propiedades';
-	public edad:number = 15;
-	public estatus:boolean = true;
-	public materias:Array<String> = ['Matematicas', 'Español', 'Historia'];
-	comodin:any = 'Lo que sea es este parametro';
+	public nombre:string;
+	public edad:number;
+	public estatus:boolean;
+	public materias:Array<String>;
+	comodin:any;
+
+	constructor(){
+		this.nombre = 'Jose Ignacio';
+		this.edad = 15;
+		this.estatus = true;
+		this.materias = ['Matematicas', 'Español', 'Historia'];
+		this.comodin = 'Lo que sea es este parametro';
+	}
+
+	ngOnInit(){
+		this.hola(undefined);
+		this.hola(this.nombre);
+		this.adios();
+		this.adios(undefined);
+		this.adios(this.nombre);
+		this.persona('Drago').holaPersona();
+	}
+
+	persona(persona:string){
+		this.nombre = persona;
+
+		return this;
+	}
+
+	holaPersona(){
+		console.log(this.nombre);
+	}
+
+	hola(persona:string = 'anonimo'){
+		console.log('Hola' + persona);
+	}
+
+	adios(persona?:string){
+		if(persona){
+			console.log('Adios' + persona);	
+			 return false;
+		}
+		console.log('Adios desconocido');	
+	}
 }
