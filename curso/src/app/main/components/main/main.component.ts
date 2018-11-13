b7958eef5e4bb2cea28e071a2bff1112723898ed
pachecoSen <pachecodr2013@gmail.com>
2018-11-12 18:58:01 -0600

    f0004. Modelo de datos

--- curso/src/app/main/components/main/main.component.ts
+++ curso/src/app/main/components/main/main.component.ts
@@ -1,38 +1,26 @@
 import { Component } from '@angular/core';
 
+import { MainModel } from './main.model';
+
 @Component({
 	selector: 'main-app-root',
 	templateUrl: './assets/main.component.html',
 	styleUrls: ['./assets/main.component.css']
 })
 export class MainComponent {
-	public nombre:string;
-	public edad:number;
-	public estatus:boolean;
-	public materias:Array<String>;
-	comodin:any;
+	public title:string;
+	public empleado:MainModel;
+	public empleado_s:Array<MainModel>;
 
 	constructor(){
-		this.nombre = 'Jose Ignacio';
-		this.edad = 15;
-		this.estatus = true;
-		this.materias = ['Matematicas', 'Español', 'Historia'];
-		this.comodin = 'Lo que sea es este parametro';
+		this.title = 'Componente Empleados';
+		this.empleado = new MainModel('Jose Pacheco', 24, true, ['Gerente']);
+		this.empleado_s = [
+			new MainModel('Ignacio Cruz', 24, true, ['Limpieza', 'Cosina']),
+			new MainModel('Ignacio Pacheco', 24, true, ['Recepcion', 'Seguridad']),
+			new MainModel('Jose Cruz', 24, true, ['Aistente General']),
+		];
 	}
 
-	ngOnInit(){
-		var n_uno = 8;
-		var n_dos = 5;
-
-		if(n_uno === 8){
-			var n_uno = 16;
-			let n_dos = 10;
-			console.log('Dentro de If');
-			console.log(n_uno);
-			console.log(n_dos);
-		}
-		console.log('Fuera de If');
-		console.log(n_uno);
-		console.log(n_dos);
-	}
+	ngOnInit(){}
 }
---
+++ curso/src/app/main/components/main/main.model.ts
@@ -0,0 +1,8 @@
+export class MainModel{
+	constructor(
+		public nombre:string,
+		public edad:number,
+		public estatus:boolean,
+		public area:Array<String>
+	){}
+}
